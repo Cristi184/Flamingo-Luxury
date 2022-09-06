5e9cd70af984c05ce9dcbf3b0195737d63bf111f
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-09-06 15:23:30 +0300

    fix

--- src/Main/Pages/Home/Components/Location/MyMap.style.js
+++ src/Main/Pages/Home/Components/Location/MyMap.style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import background from "./image/background.png"
+import background from "./image/background.jpg"
 
 export const TextContainer = styled.div`
   margin-top: 40px;
