f12e9534a84b2315186eddd63d7c5e032343e819
cristian.stan1 <cristian.stan@ivfuture.uk>
2022-09-07 09:08:21 +0300

    add Link

--- src/Main/Pages/Home/Components/Navbar/DropButton/DropButton.js
+++ src/Main/Pages/Home/Components/Navbar/DropButton/DropButton.js
@@ -1,8 +1,9 @@
 import './DropButton.css'
+import {Link} from "react-router-dom";
 
 const DropButton = (props) => {
     const list = props.list
-    const listItems = list.map((item, key) => <a key={Math.random()} href={'/' + item}>{item}</a>)
+    const listItems = list.map((item, key) => <Link key={Math.random()} to={'/' + item}>{item}</Link>)
     return (
         <div className="dropdown">
             <button className="dropbtn">{props.title}</button>
